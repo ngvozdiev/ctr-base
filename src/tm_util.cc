4e3a15a8e2df0d726ef63f312c0097e8d6b1d4e7
Nik <nik.gvozdiev@gmail.com>
2017-08-18 10:52:21 +0100

    Updates to tm_util

--- src/tm_util.cc
+++ src/tm_util.cc
@@ -11,24 +11,27 @@
 
 DEFINE_string(topology, "", "A file with a topology");
 DEFINE_string(traffic_matrix, "", "A file with a traffic matrix");
-DEFINE_double(tm_scale, 1.0, "The traffic matrix will be scaled by this much");
+DEFINE_double(max_demand_to_max_link_capacity, 0.5,
+              "The fraction max_demand / max_link_capacity");
 DEFINE_double(
     target_commodity_scale_factor, 1.0,
     "The matrix should be feasible if all commodities are scaled by this much");
 DEFINE_uint64(seed, 1, "Seed for the RNG");
+DEFINE_string(output, "", "If non-empty will save the output matrix there");
 
-static std::unique_ptr<nc::lp::DemandMatrix> MakeFitRecursive(
+static void MakeFitRecursive(
     const nc::lp::DemandMatrix& input,
     const std::vector<nc::lp::DemandMatrixElement>& all_elements, size_t from,
-    size_t to, double target_commodity_scale_factor) {
+    size_t to, double target_csf,
+    std::unique_ptr<nc::lp::DemandMatrix>* best_so_far, double* best_csf) {
   size_t range = (to - from) / 2;
   size_t threshold = from + range;
   if (threshold == 0) {
-    return {};
+    return;
   }
 
   if (threshold == all_elements.size()) {
-    return {};
+    return;
   }
 
   std::set<nc::lp::DemandMatrix::NodePair> to_remove;
@@ -40,35 +43,39 @@ static std::unique_ptr<nc::lp::DemandMatrix> MakeFitRecursive(
   std::unique_ptr<nc::lp::DemandMatrix> candidate =
       input.RemovePairs(to_remove);
   if (range == 0) {
-    return candidate;
+    return;
   }
 
   if (candidate->IsFeasible({})) {
-    double sf = candidate->MaxCommodityScaleFractor();
-    LOG(ERROR) << "T " << threshold << "/" << all_elements.size() << " sf "
-               << sf;
-    if (std::abs(sf - target_commodity_scale_factor) < 0.01) {
-      return candidate;
+    double csf = candidate->MaxCommodityScaleFractor();
+    if (std::abs(*best_csf - target_csf) > std::abs(csf - target_csf)) {
+      *best_so_far = std::move(candidate);
+      *best_csf = csf;
     }
 
-    if (sf > target_commodity_scale_factor) {
-      return MakeFitRecursive(input, all_elements, from, threshold,
-                              target_commodity_scale_factor);
-    } else {
-      return MakeFitRecursive(input, all_elements, threshold, to,
-                              target_commodity_scale_factor);
+    if (std::abs(csf - target_csf) < 0.01) {
+      return;
     }
+
+    if (csf > target_csf) {
+      MakeFitRecursive(input, all_elements, from, threshold, target_csf,
+                       best_so_far, best_csf);
+    } else {
+      MakeFitRecursive(input, all_elements, threshold, to, target_csf,
+                       best_so_far, best_csf);
+    }
+
+    return;
   }
 
-  LOG(ERROR) << "T " << threshold << "/" << all_elements.size() << " NF";
-  return MakeFitRecursive(input, all_elements, threshold, to,
-                          target_commodity_scale_factor);
+  MakeFitRecursive(input, all_elements, threshold, to, target_csf, best_so_far,
+                   best_csf);
 }
 
 // Removes small commodities until the TM fits with a given
 // commodity_scale_factor.
 static std::unique_ptr<nc::lp::DemandMatrix> MakeFit(
-    const nc::lp::DemandMatrix& input, double target_commodity_scale_factor) {
+    const nc::lp::DemandMatrix& input, double target_csf) {
   // Demands, ordered by size.
   std::vector<nc::lp::DemandMatrixElement> all_elements = input.elements();
 
@@ -76,8 +83,27 @@ static std::unique_ptr<nc::lp::DemandMatrix> MakeFit(
   std::shuffle(all_elements.begin(),
                std::next(all_elements.begin(), all_elements.size()), rnd);
 
-  return MakeFitRecursive(input, all_elements, 0, all_elements.size(),
-                          target_commodity_scale_factor);
+  nc::net::Bandwidth max_demand;
+  for (const auto& element : all_elements) {
+    max_demand = std::max(max_demand, element.demand);
+  }
+
+  nc::net::Bandwidth max_link_capacity;
+  const nc::net::GraphStorage* graph = input.graph();
+  for (nc::net::GraphLinkIndex link_index : graph->AllLinks()) {
+    max_link_capacity =
+        std::max(max_link_capacity, graph->GetLink(link_index)->bandwidth());
+  }
+
+  double scale =
+      FLAGS_max_demand_to_max_link_capacity * (max_link_capacity / max_demand);
+  auto scaled_input = input.Scale(scale);
+
+  std::unique_ptr<nc::lp::DemandMatrix> best_candidate;
+  double best_csf = std::numeric_limits<double>::max();
+  MakeFitRecursive(*scaled_input, all_elements, 0, all_elements.size(),
+                   target_csf, &best_candidate, &best_csf);
+  return best_candidate;
 }
 
 int main(int argc, char** argv) {
@@ -93,11 +119,15 @@ int main(int argc, char** argv) {
       nc::lp::DemandMatrix::LoadRepetitaOrDie(
           nc::File::ReadFileToStringOrDie(FLAGS_traffic_matrix), node_order,
           &graph);
-  demand_matrix = demand_matrix->Scale(FLAGS_tm_scale);
   demand_matrix = MakeFit(*demand_matrix, FLAGS_target_commodity_scale_factor);
 
   LOG(INFO) << graph.Stats().ToString();
   if (demand_matrix) {
     LOG(INFO) << demand_matrix->ToString();
+  }
+
+  if (!FLAGS_output.empty()) {
+    std::string serialized_matrix = demand_matrix->ToRepetita(node_order);
+    nc::File::WriteStringToFile(serialized_matrix, FLAGS_output);
   }
 }
