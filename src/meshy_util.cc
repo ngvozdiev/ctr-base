4be2a285260404115dc3658641a48bdd1ebdafa6
Nik Gvozdiev <nik.gvozdiev@gmail.com>
2017-10-18 09:18:39 +0100

    updated meshy

--- src/meshy_util.cc
+++ src/meshy_util.cc
@@ -1,7 +1,6 @@
 #include <gflags/gflags.h>
-#include <algorithm>
 #include <chrono>
-#include <ratio>
+#include <memory>
 #include <string>
 #include <tuple>
 #include <utility>
@@ -10,16 +9,14 @@
 #include "ncode_common/src/common.h"
 #include "ncode_common/src/file.h"
 #include "ncode_common/src/logging.h"
+#include "ncode_common/src/lp/mc_flow.h"
 #include "ncode_common/src/net/algorithm.h"
 #include "ncode_common/src/net/net_common.h"
 #include "ncode_common/src/net/net_gen.h"
+#include "ncode_common/src/perfect_hash.h"
 #include "ncode_common/src/strutil.h"
-#include "ncode_common/src/viz/graph.h"
-#include "ncode_common/src/viz/grapher.h"
-#include "ncode_common/src/viz/web_page.h"
 
 DEFINE_string(topology_files, "", "Topology files");
-DEFINE_double(delay_scale, 1.3, "By how much to scale the delays of all links");
 
 static std::vector<std::string> GetTopologyFiles() {
   std::vector<std::string> out;
@@ -30,24 +27,6 @@ static std::vector<std::string> GetTopologyFiles() {
   }
 
   return out;
-}
-
-static void DumpGraphToHTML(const std::string& out,
-                            const nc::net::GraphStorage& graph) {
-  std::vector<nc::viz::EdgeData> edges;
-  for (nc::net::GraphLinkIndex link : graph.AllLinks()) {
-    const nc::net::GraphLink* link_ptr = graph.GetLink(link);
-    double delay_ms =
-        std::chrono::duration<double, std::milli>(link_ptr->delay()).count();
-
-    std::vector<double> loads = {1.0};
-    edges.emplace_back(link, loads, link_ptr->ToStringNoPorts(), delay_ms);
-  }
-
-  nc::viz::HtmlPage page;
-  nc::viz::DisplayMode display_mode("default");
-  nc::viz::GraphToHTML(edges, {}, {display_mode}, graph, &page);
-  nc::File::WriteStringToFile(page.Construct(), out);
 }
 
 // Returns true if two nodes are reachable given a set of constraints.
@@ -105,9 +84,9 @@ static nc::net::Bandwidth PairwiseFlow(nc::net::GraphNodeIndex source_one,
 
 // Returns the fraction of all pairs that are reachable when the shortest path
 // between two nodes in excluded from the graph.
-static double ReachableFraction(nc::net::GraphNodeIndex node_one,
-                                nc::net::GraphNodeIndex node_two,
-                                const nc::net::GraphStorage& graph) {
+static std::pair<double, double> ReachableFraction(
+    nc::net::GraphNodeIndex node_one, nc::net::GraphNodeIndex node_two,
+    const nc::net::GraphStorage& graph) {
   double total = 0;
   double reachable = 0;
   double unreachable_no_alternative = 0;
@@ -130,6 +109,10 @@ static double ReachableFraction(nc::net::GraphNodeIndex node_one,
         continue;
       }
 
+      if (src == node_one && dst == node_two) {
+        continue;
+      }
+
       ++total;
       if (Reachable(src, dst, graph, exclusion_set)) {
         ++reachable;
@@ -143,7 +126,25 @@ static double ReachableFraction(nc::net::GraphNodeIndex node_one,
     }
   }
 
-  return reachable / total;
+  return {reachable / total, unreachable_no_alternative / total};
+}
+
+static void ComputeReachableFractions(const nc::net::GraphStorage& graph) {
+  nc::net::GraphNodeSet all_nodes = graph.AllNodes();
+  for (nc::net::GraphNodeIndex src : all_nodes) {
+    for (nc::net::GraphNodeIndex dst : all_nodes) {
+      if (src <= dst) {
+        continue;
+      }
+
+      double reachable;
+      double unreachable;
+      std::tie(reachable, unreachable) = ReachableFraction(src, dst, graph);
+      LOG(INFO) << reachable << " " << unreachable << " "
+                << graph.GetNode(src)->id() << " " << graph.GetNode(dst)->id();
+      return;
+    }
+  }
 }
 
 int main(int argc, char** argv) {
@@ -157,34 +158,7 @@ int main(int argc, char** argv) {
     nc::net::GraphBuilder builder = nc::net::LoadRepetitaOrDie(
         nc::File::ReadFileToStringOrDie(topology_file));
     builder.RemoveMultipleLinks();
-    builder.ScaleDelay(FLAGS_delay_scale);
     nc::net::GraphStorage graph(builder);
-
-    const nc::net::GraphLink* max_delay_link = MaxDelayLink(graph);
-    if (graph.AllNodes().Count() < 10) {
-      continue;
-    }
-
-    size_t links_in_tree = graph.AllNodes().Count() - 1;
-    size_t bidirectional_links = graph.AllLinks().Count() / 2;
-    double links_to_remove = bidirectional_links - links_in_tree;
-    all_links_to_remove.emplace_back(links_to_remove / bidirectional_links);
-
-    LOG(INFO) << topology_file << " " << graph.AllNodes().Count() << " "
-              << graph.AllLinks().Count() << " max delay link "
-              << max_delay_link->ToStringNoPorts() << " delay "
-              << max_delay_link->delay().count();
-
-    if (topology_files.size() == 1) {
-      DumpGraphToHTML("out.html", graph);
-    }
-  }
-
-  if (topology_files.size() > 1) {
-    nc::viz::DataSeries1D data_1d;
-    data_1d.data = std::move(all_links_to_remove);
-
-    nc::viz::PythonGrapher python_grapher("meshy_ltr");
-    python_grapher.PlotCDF({}, {data_1d});
+    ComputeReachableFractions(graph);
   }
 }
