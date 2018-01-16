6860a8ef0298ed89a9d62b093f2b38e1a1c7d0aa
Nik <nik.gvozdiev@gmail.com>
2018-01-16 22:46:33 +0000

    updated path counting

--- src/dump_aggregate_cost.cc
+++ src/dump_aggregate_cost.cc
@@ -1,53 +1,34 @@
 #include <gflags/gflags.h>
-#include <ncode/logging.h>
-#include <ncode/net/algorithm.h>
-#include <ncode/net/net_common.h>
+#include <ncode/file.h>
 #include <ncode/viz/grapher.h>
-#include <stddef.h>
-#include <algorithm>
-#include <chrono>
+#include <cstdint>
+#include <iostream>
 #include <map>
 #include <memory>
-#include <random>
-#include <utility>
 #include <vector>
 
 #include "common.h"
-#include "opt/opt.h"
 #include "topology_input.h"
+#include "opt/opt.h"
 
-DEFINE_string(sp_fractions, "1.2", "How far from the SP a path can be");
-
-static std::vector<double> GetCommaSeparated(const std::string& value) {
-  std::vector<std::string> split = nc::Split(value, ",");
-  std::vector<double> out;
-  for (const auto& piece : split) {
-    double v;
-    CHECK(nc::safe_strtod(piece, &v));
-    out.emplace_back(v);
-  }
-
-  return out;
-}
+DEFINE_double(sp_fraction, 1.2, "How far from the SP a path can be");
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+  nc::viz::CDFPlot path_counts_plot;
   std::vector<ctr::TopologyAndFilename> topologies = ctr::GetTopologyInputs();
-  CHECK(topologies.size() == 1) << "Too many topologies";
-
-  nc::viz::CDFPlot capacities_plot;
-  const nc::net::GraphStorage* graph = topologies[0].graph.get();
-  std::vector<double> values = GetCommaSeparated(FLAGS_sp_fractions);
-  for (double v : values) {
+  for (const auto& topology_and_filename : topologies) {
+    const nc::net::GraphStorage& graph = *topology_and_filename.graph;
     std::map<ctr::AggregateId, uint64_t> counts_at_delay =
-        ctr::GetPathCountsAtDelay(*graph, v);
-    std::vector<double> capacities;
-    for (const auto& aggregate_and_capacity : counts_at_delay) {
-      capacities.emplace_back(aggregate_and_capacity.second);
+        ctr::GetPathCountsAtDelay(graph, FLAGS_sp_fraction);
+    std::vector<double> path_counts;
+    for (const auto& aggregate_and_path_count : counts_at_delay) {
+      path_counts.emplace_back(aggregate_and_path_count.second);
     }
 
-    capacities_plot.AddData(nc::StrCat("fraction ", v), capacities);
+    std::string label = nc::File::ExtractFileName(topology_and_filename.file);
+    path_counts_plot.AddData(label, path_counts);
   }
 
-  capacities_plot.PlotToDir("capacities_out");
+  path_counts_plot.PlotToDir("path_counts_out");
 }
